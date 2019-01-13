679a63da754dbe4ace7673793cee381d86396001
andihoerudin24 <andihoerudin24@gmail.com>
2019-01-13 17:11:05 +0700

    13 Factory and Seed

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -1,9 +1,12 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\User;
 
 $factory->define(App\Model\Like::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function(){
+            return User::all()->random();
+        }
     ];
 });
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -1,9 +1,20 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\User;
+use App\Model\Category;
 
 $factory->define(App\Model\Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title'=>$title,
+        'slug'=>str_slug($title),
+        'body'=>$faker->text,
+        'category_id' => function(){
+            return Category::all()->random();
+        },
+        'user_id' => function(){
+            return User::all()->random();
+        }
     ];
 });
