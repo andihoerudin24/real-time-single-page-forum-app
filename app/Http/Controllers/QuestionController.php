44122e8356c50f535d711a3c73cd03b9390b46f1
andihoerudin24 <andihoerudin24@gmail.com>
2019-01-14 22:10:28 +0700

    CRUD FOR QUESTION along with  Resource Transformation

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Model\Question;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\QuestionResource;
 
 class QuestionController extends Controller
 {
@@ -14,7 +16,7 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+        return QuestionResource::collection(Question::latest()->get());
     }
 
     /**
@@ -35,7 +37,9 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        auth()->user()->question()->create($request->all());
+        //Question::create($request->all());
+        return response('Created',Response::HTTP_CREATED);
     }
 
     /**
@@ -46,19 +50,10 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
+
+        return new QuestionResource($question);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -80,6 +75,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,13 +3,23 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Model\Category;
+use App\Model\Reply;
 
 class Question extends Model
 {
+    //protected $fillable=['title','slug','body','category_id','user_id'];
+     protected $guarded=[];
+
+    public function getRouteKeyName()
+    {
+         return 'slug';
+    }
+
     public function user()
     {
-
-         return $this->belongsTo(User::class, 'user_id','id');
+      return $this->belongsTo(User::class, 'user_id','id');
    }
 
 
@@ -22,6 +32,11 @@ class Question extends Model
    public function cateegory()
    {
        return $this->belongsTo(Category::class,'category_id','id');
+   }
+
+   public function getPathAttribute()
+   {
+       return asset("api/question/$this->slug");
    }
 
 
